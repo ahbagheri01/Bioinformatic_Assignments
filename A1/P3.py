0afe536e4195280fca45863d7cf206f4aa041870
Amirhossein <ahbaghero01@gmail.com>
2022-10-28 17:37:52 +0330

    added all

--- A1/P3.py
+++ A1/P3.py
@@ -5,6 +5,20 @@ alphabet = lines[0].strip().split(" ")
 n = int(lines[1])
 base = len(alphabet)
 f = open("A3.txt","w")
-for x in itertools.product(alphabet, repeat=n):
-    f.write(''.join(x))
+def numberToBase(n, b , l):
+    if n == 0:
+        return [0]
+    digits = [0] * l
+    count = 0
+    while n:
+        digits[l - count- 1] = int(n%b)
+        count += 1
+        n //= b
+    return digits
+f.write(f"{alphabet[0]}"*n+"\n")
+for i in range(1,base ** n):
+    f.write(''.join([alphabet[index] for index in numberToBase(i, base, n)]))
     f.write("\n")
+# for x in itertools.product(alphabet, repeat=n):
+#     f.write(''.join(x))
+#     f.write("\n")
